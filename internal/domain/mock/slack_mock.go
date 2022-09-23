3fc362212c36f786e5435c3f34ee20342b3d6327
vietanhb <nguyenvietanhb@sun-asterisk.com>
2022-09-23 16:39:06 +0700

    Make a function is used to send message

--- internal/domain/mock/slack_mock.go
+++ internal/domain/mock/slack_mock.go
@@ -43,8 +43,23 @@ func (m *MockSlackUsecase) HandleAppMentionEvent(arg0 context.Context, arg1 *dom
 	return ret0
 }
 
+// SendMessage mocks base method.
+func (m *MockSlackUsecase) SendMessage(ctx context.Context, channel string, content string, mentionUser string) (string, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "SendMessage", ctx, channel, content, mentionUser)
+	ret0, _ := ret[0].(string)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
 // HandleAppMentionEvent indicates an expected call of HandleAppMentionEvent.
 func (mr *MockSlackUsecaseMockRecorder) HandleAppMentionEvent(arg0, arg1 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "HandleAppMentionEvent", reflect.TypeOf((*MockSlackUsecase)(nil).HandleAppMentionEvent), arg0, arg1)
+}
+
+// SendMessage indicates an expected call of SendMessage.
+func (mr *MockSlackUsecaseMockRecorder) SendMessage(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SendMessage", reflect.TypeOf((*MockSlackUsecase)(nil).SendMessage), arg0, arg1)
 }
